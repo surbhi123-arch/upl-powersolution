6f71f3e24702b89c48cba49dff3a9b23aed3a850
Surbhi khedkar <surabhikhedkar55@gmail.com>
2026-07-08 17:26:27 +0530

    Made website mobile responsive

--- js/script.js
+++ js/script.js
@@ -164,6 +164,8 @@ scrollBtn.addEventListener("click", () => {
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-});
+if(menuToggle){
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
